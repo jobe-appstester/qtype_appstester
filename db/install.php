de46e4b373dc1803ca1fb3e96c9f2b7d793e9c1e
Ilya Gavrilov <42314637+devgavrilov@users.noreply.github.com>
2021-12-02 22:07:03 +0300

    FIx installation function.

--- db/install.php
+++ db/install.php
@@ -5,7 +5,7 @@ require_once(__DIR__.'/common.php');
 /**
  * @throws ddl_exception
  */
-function xmldb_qtype_appstester_install($oldversion): bool
+function xmldb_qtype_appstester_install(): bool
 {
     return ensure_database();
 }

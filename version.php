70f33557462ce9ab3e6c8439d8e9552366afc586
Ilya Gavrilov <42314637+devgavrilov@users.noreply.github.com>
2021-12-10 23:26:09 +0300

    All the plugin meta information.

--- version.php
+++ version.php
@@ -2,6 +2,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2021120223;
 $plugin->component = 'qtype_appstester';
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->version = 2021121000;
+$plugin->maturity = MATURITY_BETA;
+$plugin->release = 'v0.01';
+$plugin->dependencies = array(
+    'qbehaviour_appstester' => 2021121000
+);

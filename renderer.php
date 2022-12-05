d30953eab391699a8e3a9dae4825e54369d20596
semen.lavren <semen.lavren@partner.samsung.com>
2022-12-05 14:23:06 +0300

    Added results render on finishing step

--- renderer.php
+++ renderer.php
@@ -32,14 +32,28 @@ class qtype_appstester_renderer extends qtype_renderer {
         $checker_system_name = $qa->get_question()->checker_system_name;
         $checker = checker_definitions_registry::get_by_system_name($checker_system_name);
 
-        if ($qa->get_last_step()->has_behaviour_var('result')) {
-            $result = json_decode($qa->get_last_step()->get_qt_var('-result'), true);
-            return $checker->render_result_feedback($result);
-        }
+        if ($qa->get_state()->is_active()) {
+            if ($qa->get_last_step()->has_behaviour_var('result')) {
+                $result = json_decode($qa->get_last_step()->get_qt_var('-result'), true);
+                return $checker->render_result_feedback($result);
+            }
 
-        if ($qa->get_last_step()->has_behaviour_var('status')) {
-            $status = json_decode($qa->get_last_step()->get_qt_var('-status'), true);
-            return $checker->render_status_feedback($status);
+            if ($qa->get_last_step()->has_behaviour_var('status')) {
+                $status = json_decode($qa->get_last_step()->get_qt_var('-status'), true);
+                return $checker->render_status_feedback($status);
+            }
+        } else {
+            $laststepwithresult = $qa->get_last_step_with_behaviour_var('result');
+            if ($laststepwithresult->get_state() !== question_state::$unprocessed) {
+                $result = json_decode($laststepwithresult->get_qt_var('-result'), true);
+                return $checker->render_result_feedback($result);
+            }
+
+            $laststepwithstatus= $qa->get_last_step_with_behaviour_var('status');
+            if ($laststepwithstatus->get_state() !== question_state::$unprocessed) {
+                $status = json_decode($laststepwithstatus->get_qt_var('-status'), true);
+                return $checker->render_status_feedback($status);
+            }
         }
 
         return '';
